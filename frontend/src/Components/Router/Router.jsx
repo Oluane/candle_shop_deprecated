030622c18a786ded816fbd5b41d86971a85e2fe6
Roche Pauline <rochepauline6@gmail.com>
2020-08-03 20:05:01 +0200

    improving privates routes & redirections

--- frontend/src/Components/Router/Router.jsx
+++ frontend/src/Components/Router/Router.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { routes } from "../../services/routes";
@@ -6,12 +6,35 @@ import Navbar from "../Navbar/Navbar";
 import apiInstance from "../../services/api";
 import userActions from "../../redux/actions/userActions";
 
-const PrivateRoute = ({ component: Component, auth, ...rest }) => (
+const PrivateRoute = ({ component: Component, auth, authenticationRequestState, ...rest }) => (
 	<Route
 		{...rest}
-		render={(props) =>
-			auth === true ? <Component {...props} /> : <Redirect to="/account/login" />
-		}
+		render={(props) => {
+			switch (auth) {
+				case true:
+					return <Component {...props} />;
+				case false:
+					switch (authenticationRequestState) {
+						case "notYetAsked":
+						case "loading":
+							return null;
+						case "ok":
+							return <Component {...props} />;
+						case "invalidToken":
+						case "noToken":
+							return (
+								<Redirect
+									to={{
+										pathname: "/account/login",
+										state: { from: props.location.pathname },
+									}}
+								/>
+							);
+						default:
+							return null;
+					}
+			}
+		}}
 	/>
 );
 
@@ -19,25 +42,38 @@ const Router = () => {
 	const dispatch = useDispatch();
 
 	const isLoggedUser = useSelector((state) => state.user.isLoggedIn);
+	const [authenticationRequestState, setAuthenticationRequestState] = useState("notYetAsked");
 
 	useEffect(() => {
-		let xsrfToken = null;
-		xsrfToken = localStorage.getItem("xsrfToken");
+		if (!isLoggedUser) {
+			let xsrfToken = null;
+			xsrfToken = localStorage.getItem("xsrfToken");
 
-		if (xsrfToken !== null) {
-			apiInstance
-				.get("/user")
-				.then(({ data }) => {
-					dispatch({ ...userActions.USER_LOGIN, payload: data[0] });
-				})
-				.catch((err) => console.log(err));
+			if (xsrfToken !== null) {
+				setAuthenticationRequestState("loading");
+				apiInstance
+					.get("/user")
+					.then(({ data }) => {
+						dispatch({ ...userActions.USER_LOGIN, payload: data[0] });
+						setAuthenticationRequestState("ok");
+					})
+					.catch((err) => {
+						if (err.response.status === 401) {
+							setAuthenticationRequestState("invalidToken");
+						} else {
+							setAuthenticationRequestState("internalError");
+						}
+					});
+			} else {
+				setAuthenticationRequestState("noToken");
+			}
 		}
-	}, []);
+	}, [isLoggedUser]);
 
 	return (
 		<>
 			<Navbar />
-			<main style={{ marginTop: "50px" }}>
+			<main style={{ marginTop: "65px" }}>
 				<Switch>
 					{Object.keys(routes).map((route, key) => {
 						const { path, component, isPrivate } = routes[route];
@@ -52,10 +88,12 @@ const Router = () => {
 									component={component}
 									auth={isLoggedUser}
 									key={key}
+									authenticationRequestState={authenticationRequestState}
 								/>
 							);
 						}
 					})}
+					<Redirect from="*" to="/" />
 				</Switch>
 			</main>
 		</>
