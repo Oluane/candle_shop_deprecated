88c4e2f3d5243f0ba9f9d383d5daafe622213648
Roche Pauline <rochepauline6@gmail.com>
2020-10-07 18:12:03 +0200

    delete/adding addresses in my addresses section

--- frontend/src/Components/MyAddresses/MyAddresses.jsx
+++ frontend/src/Components/MyAddresses/MyAddresses.jsx
@@ -1,106 +1,149 @@
 import "./MyAddresses.scss";
 
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import AddressForm from "../AddressForm/AddressForm";
+import { ToastContext } from "../Toasts/ToastProvider";
+import addressesActions from "../../redux/actions/addressesActions";
+import apiInstance from "../../services/api/api";
 
 const MyAddresses = () => {
 	const userAddresses = useSelector((state) => state.addresses);
+	const currentUser = useSelector((state) => state.user.data);
 	const dispatch = useDispatch();
+	const [, dispatchToast] = useContext(ToastContext);
 
 	const [showEditingAddressId, setShowEditingAddressId] = useState(null);
-	const [favAddressIdx, setFavAddressIdx] = useState(null);
+	const [showAddingAddress, setShowAddingAddress] = useState(false);
+
+	let favAddress = userAddresses.find((address) => address.isFavorite === 1);
+	let otherAddresses = userAddresses.filter(
+		(address) => favAddress && address.id !== favAddress.id
+	);
 
 	useEffect(() => {
-		if (userAddresses[0] !== -1) {
-			const favIdx = userAddresses.findIndex((address) => address.isFavorite === 1);
-
-			if (favIdx !== -1) {
-				setFavAddressIdx(favIdx);
-			}
-		}
+		setShowEditingAddressId(null);
+		setShowAddingAddress(false);
 	}, [userAddresses]);
 
-	const deleteAddress = (addressId) => {};
+	const deleteAddress = (addressId) => {
+		apiInstance
+			.delete(`/user/${currentUser.id}/address/${addressId}`)
+			.then(() => {
+				dispatch({
+					...addressesActions.ADDRESSES_DELETE_ONE,
+					payload: { addressId: addressId },
+				});
+
+				dispatchToast({
+					type: "ADD_TOAST",
+					payload: {
+						id: "toast " + Date.now(),
+						status: "success",
+						text: "Address deleted.",
+					},
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+				dispatchToast({
+					type: "ADD_TOAST",
+					payload: {
+						id: "toast " + Date.now(),
+						status: "success",
+						text: "A problem occured trying to delete address. Try again :) ",
+					},
+				});
+			});
+	};
 
 	return (
 		<div className="myAddresses">
 			<h2 className="sectionTitle">MY ADDRESSES</h2>
 			<div className="mainWrapper">
-				{favAddressIdx !== null && (
+				{userAddresses.length < 5 && (
+					<div className="addingNewAddressWrapper">
+						{!showAddingAddress ? (
+							<button
+								className="mediumBold mediumText"
+								onClick={() => {
+									setShowAddingAddress(true);
+									setShowEditingAddressId(null);
+								}}
+							>
+								ADD A NEW ADDRESS
+							</button>
+						) : (
+							<>
+								<h4 className="alignCenter">ADD A NEW ADDRESS</h4>
+								<AddressForm />
+							</>
+						)}
+					</div>
+				)}
+				{favAddress !== undefined && (
 					<div className="addressWrapper">
-						<h4 className="mediumBold">
-							My main address{" "}
-							<span>
-								{userAddresses[favAddressIdx].name &&
-									userAddresses[favAddressIdx].name}
-							</span>
-						</h4>
+						<h4 className="mediumBold">My main address </h4>
 						<div className="mainAddressDisplay usualText">
-							<p>{userAddresses[favAddressIdx].address}</p>
+							<p className="mediumBold">{favAddress.name && favAddress.name}</p>
+							<p>{favAddress.address}</p>
+							<p>{favAddress.addressComplement && favAddress.addressComplement}</p>
 							<p>
-								{userAddresses[favAddressIdx].addressComplement &&
-									userAddresses[favAddressIdx].addressComplement}
-							</p>
-							<p>
-								{userAddresses[favAddressIdx].zipCode}{" "}
-								{userAddresses[favAddressIdx].city}
+								{favAddress.zipCode} {favAddress.city}
 							</p>
 						</div>
 						<div className="addressesActions">
 							<button
 								onClick={() => {
-									setShowEditingAddressId(userAddresses[favAddressIdx].id);
+									setShowEditingAddressId(favAddress.id);
+									setShowAddingAddress(false);
 								}}
 							>
 								Edit
 							</button>
-							<button>Delete</button>
+							<button onClick={() => deleteAddress(favAddress.id)}>Delete</button>
 						</div>
-						{showEditingAddressId === userAddresses[favAddressIdx].id && (
+						{showEditingAddressId === favAddress.id && (
 							<div className="editingForm">
 								<h4 className="alignCenter">EDIT MY ADDRESS</h4>
-								<AddressForm addressObj={userAddresses[favAddressIdx]} />
+								<AddressForm addressObj={favAddress} />
 							</div>
 						)}
 					</div>
 				)}
 
-				{userAddresses.map((address, i) => {
-					if (i !== favAddressIdx) {
-						return (
-							<div className="addressWrapper">
-								<h4 className="mediumBold">
-									Other address
-									<span>{address.name && address.name}</span>
-								</h4>
-								<div className="mainAddressDisplay usualText">
-									<p>{address.address}</p>
-									<p>{address.addressComplement && address.addressComplement}</p>
-									<p>
-										{address.zipCode} {address.city}
-									</p>
-								</div>
-								<div className="addressesActions">
-									<button
-										onClick={() => {
-											setShowEditingAddressId(address.id);
-										}}
-									>
-										Edit
-									</button>
-									<button>Delete</button>
-								</div>
-								{showEditingAddressId === address.id && (
-									<div className="editingForm">
-										<h4 className="alignCenter">EDIT MY ADDRESS</h4>
-										<AddressForm addressObj={address} />
-									</div>
-								)}
+				{otherAddresses.map((address, i) => {
+					return (
+						<div className="addressWrapper" key={"address" + address.id}>
+							<h4 className="mediumBold">Address {i + 1}</h4>
+							<div className="mainAddressDisplay usualText">
+								<p className="mediumBold">{address.name && address.name}</p>
+								<p>{address.address}</p>
+								<p>{address.addressComplement && address.addressComplement}</p>
+								<p>
+									{address.zipCode} {address.city}
+								</p>
 							</div>
-						);
-					}
+							<div className="addressesActions">
+								<button
+									onClick={() => {
+										setShowEditingAddressId(address.id);
+										setShowAddingAddress(false);
+									}}
+								>
+									Edit
+								</button>
+								<button onClick={() => deleteAddress(address.id)}>Delete</button>
+							</div>
+							{showEditingAddressId === address.id && (
+								<div className="editingForm">
+									<h4 className="alignCenter">EDIT MY ADDRESS</h4>
+									<AddressForm addressObj={address} />
+								</div>
+							)}
+						</div>
+					);
 				})}
 			</div>
 		</div>
