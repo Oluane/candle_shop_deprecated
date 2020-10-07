dfde4e709b90ad355394c30a4fe75176d01a667a
Roche Pauline <rochepauline6@gmail.com>
2020-10-07 18:09:09 +0200

    updating put/post address endpoints to include stored proc usage

--- backend/routes/address.js
+++ backend/routes/address.js
@@ -25,49 +25,91 @@ router.get("/", isAuthenticated, (req, res) => {
 	);
 });
 
-router.post("/", isAuthenticated, (req, res) => {
-	const userId = req.params.userId;
-	const newAddressData = req.body;
+router.post(
+	"/",
+	isAuthenticated,
+	(req, res, next) => {
+		const userId = req.params.userId;
+		const newAddressData = req.body;
 
-	if (!userId) {
-		return res.status(401).json({ message: "OOPS! Missing userId" });
-	}
-
-	db.query(`INSERT INTO customer_address SET ?`, [newAddressData], (err, results) => {
-		if (err) {
-			console.log(err);
-			return res.status(500).json({ message: "Internal Error" });
+		if (!userId) {
+			return res.status(401).json({ message: "OOPS! Missing userId" });
 		}
-		res.status(200).json(results);
-	});
-});
 
-router.put("/:addressId", isAuthenticated, (req, res) => {
-	const addressId = req.params.addressId;
-	const userId = req.params.userId;
-	const addressData = req.body;
+		if (newAddressData.is_favorite === 1) {
+			db.query(`CALL ResetFavAddress(?, ?)`, [userId, 0], (err, results) => {
+				if (err) {
+					console.log(err);
+					return res.status(500).json({ message: "Internal Error" });
+				}
+				next();
+			});
+		} else {
+			next();
+		}
+	},
+	(req, res) => {
+		const userId = req.params.userId;
+		const newAddressData = req.body;
 
-	if (!addressId) {
-		return res.status(401).json({ message: "OOPS! Missing addressId" });
-	}
+		newAddressData.customer_id = userId;
 
-	if (!userId) {
-		return res.status(401).json({ message: "OOPS! Missing userId" });
-	}
-
-	db.query(
-		`UPDATE customer_address ca SET ? WHERE ca.customer_id = ? AND ca.id = ?;`,
-		[addressData, userId, addressId],
-		(err, results) => {
+		db.query(`INSERT INTO customer_address SET ?`, [newAddressData], (err, results) => {
 			if (err) {
 				console.log(err);
 				return res.status(500).json({ message: "Internal Error" });
-			} else {
-				res.status(204).send();
 			}
+			res.status(200).json(results);
+		});
+	}
+);
+
+router.put(
+	"/:addressId",
+	isAuthenticated,
+	(req, res, next) => {
+		const addressId = req.params.addressId;
+		const userId = req.params.userId;
+		const addressData = req.body;
+
+		if (!addressId) {
+			return res.status(401).json({ message: "OOPS! Missing addressId" });
 		}
-	);
-});
+
+		if (!userId) {
+			return res.status(401).json({ message: "OOPS! Missing userId" });
+		}
+
+		if (addressData.is_favorite === 1) {
+			db.query(`CALL ResetFavAddress(?, ?)`, [userId, addressId], (err, results) => {
+				if (err) {
+					console.log(err);
+					return res.status(500).json({ message: "Internal Error" });
+				}
+				next();
+			});
+		} else {
+			next();
+		}
+	},
+	(req, res) => {
+		const addressId = req.params.addressId;
+		const userId = req.params.userId;
+		const addressData = req.body;
+		db.query(
+			`UPDATE customer_address ca SET ? WHERE ca.customer_id = ? AND ca.id = ?;`,
+			[addressData, userId, addressId],
+			(err, results) => {
+				if (err) {
+					console.log(err);
+					return res.status(500).json({ message: "Internal Error" });
+				} else {
+					res.status(204).send();
+				}
+			}
+		);
+	}
+);
 
 router.delete("/:addressId", isAuthenticated, (req, res) => {
 	const addressId = req.params.addressId;
